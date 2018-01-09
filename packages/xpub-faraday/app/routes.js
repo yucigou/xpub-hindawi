2aef634b30e1e84726ba62a5c3b46848ec1bc073
Bogdan Cochior <bogdan.cochior@thinslices.com>
2018-01-09 16:01:43 +0200

    Try linting precommit with new setup

--- packages/xpub-faraday/app/routes.js
+++ packages/xpub-faraday/app/routes.js
@@ -4,19 +4,19 @@ import { Route } from 'react-router-dom'
 import App from 'pubsweet-component-xpub-app/src/components'
 
 import {
-    PrivateRoute,
-    LoginPage,
-    LogoutPage,
+  PrivateRoute,
+  LoginPage,
+  LogoutPage,
 } from 'pubsweet-component-xpub-authentication/src/components'
 
 import DashboardPage from 'pubsweet-component-xpub-dashboard/src/components/DashboardPage'
 
 const Routes = () => (
-    <App>
-        <Route component={LoginPage} exact path="/login" />
-        <PrivateRoute component={DashboardPage} exact path="/" />
-        <PrivateRoute component={LogoutPage} exact path="/logout" />
-    </App>
+  <App>
+    <Route component={LoginPage} exact path="/login" />
+    <PrivateRoute component={DashboardPage} exact path="/" />
+    <PrivateRoute component={LogoutPage} exact path="/logout" />
+  </App>
 )
 
 export default Routes
