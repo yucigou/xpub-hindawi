6373fe99e03c4a590561e9706d10d77f9f3eca1d
Bogdan Cochior <bogdan.cochior@thinslices.com>
2018-01-09 15:55:26 +0200

    Change prettierrc for  precommit

--- packages/xpub-faraday/app/config/journal/metadata.js
+++ packages/xpub-faraday/app/config/journal/metadata.js
@@ -1,5 +1,5 @@
 export default {
-    issn: '2474-7394',
-    name: 'Hindawi Faraday',
-    logo: '/assets/hindawi-logo.png',
+  issn: '2474-7394',
+  name: 'Hindawi Faraday',
+  logo: '/assets/hindawi-logo.png',
 }
