790504fed182e4d43c9bdd4ba06a1b323bd30400
Alexandru Munteanu <alexandru.munteanu@thinslices.com>
2018-01-10 15:48:57 +0200

    Add dropdown and steps components

---
+++ packages/xpub-faraday/app/component-wizard/components/Steps.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { compose, withHandlers, withContext, getContext } from 'recompose'
+
+import classes from './Steps.local.scss'
+
+const Separator = () => <div className={classes.separator} />
+
+const Step = ({ title, index, currentStep }) => (
+  <div className={classes.step}>
+    {index <= currentStep && <div className={classes.bullet} />}
+    <span className={classes.stepTitle}>{`${index + 1}. ${title}`}</span>
+  </div>
+)
+
+const Steps = ({ currentStep, children, renderSeparator, renderSteps }) => (
+  <div className={classes.container}>{renderSteps()}</div>
+)
+
+const DecoratedSteps = compose(
+  withHandlers({
+    renderSteps: ({ children, currentStep }) => () => {
+      const c = []
+      React.Children.forEach(children, (child, idx) => {
+        c.push(child)
+        if (idx !== React.Children.count(children) - 1) {
+          c.push(<Separator key={Math.random()} />)
+        }
+      })
+      return c
+    },
+  }),
+  withContext({ currentStep: PropTypes.number }, ({ currentStep }) => ({
+    currentStep,
+  })),
+)(Steps)
+
+DecoratedSteps.Step = getContext({ currentStep: PropTypes.number })(Step)
+
+export default DecoratedSteps
--- packages/xpub-faraday/app/component-wizard/components/Wizard.js
+++ packages/xpub-faraday/app/component-wizard/components/Wizard.js
@@ -1,32 +1,37 @@
 import React from 'react'
-import { compose, withHandlers } from 'recompose'
+import { compose, withHandlers, withState } from 'recompose'
 import { withJournal } from 'xpub-journal'
 
-const Wizard = ({ journal: { wizard }, renderStep }) => (
+import { Dropdown, Steps } from '../'
+
+const { Step } = Steps
+
+const options = [
+  { label: 'opt 1', value: 'opt1' },
+  { label: 'caca 2', value: 'opt2' },
+  { label: 'maca 3', value: 'opt3' },
+  { label: 'vaca 4', value: 'opt4' },
+]
+
+const Wizard = ({ journal: { wizard }, getSteps, step, incrementStep }) => (
   <div>
-    {wizard.map((e, i) => (
-      <div key={Math.random()}>
-        {e.label} - {e.title}
-        {e.children && e.children.map((c, i) => renderStep(c.type))}
-      </div>
-    ))}
+    <Dropdown defaultValue={options[0]} label="My dropdown" options={options} />
+    <Dropdown defaultValue={options[1]} label="Altceva" options={options} />
+    <Dropdown defaultValue={options[3]} label="Trei" options={options} />
+    <button onClick={incrementStep}>Increment step</button>
+    <Steps currentStep={step}>
+      {getSteps().map((step, index) => (
+        <Step index={index} key={step} title={step} />
+      ))}
+    </Steps>
   </div>
 )
 
 export default compose(
-  withHandlers({
-    renderStep: props => type => {
-      switch (type) {
-        case 'dropdown':
-          return <div>render dropdown</div>
-        case 'radio':
-          return <div>render radio</div>
-        case 'checkbox':
-          return <div>render checkbox</div>
-        default:
-          return null
-      }
-    },
-  }),
   withJournal,
+  withState('step', 'changeStep', 0),
+  withHandlers({
+    getSteps: ({ journal: { wizard } }) => () => wizard.map(w => w.label),
+    incrementStep: ({ changeStep }) => () => changeStep(step => step + 1),
+  }),
 )(Wizard)
