9cc72105485037ea08263ef0db53741c9566f82b
Alexandru Munteanu <alexandru.munteanu@thinslices.com>
2018-02-01 15:59:01 +0200

    Fix lint

--- packages/components-faraday/src/components/Dashboard/redux/filters.js
+++ packages/components-faraday/src/components/Dashboard/redux/filters.js
@@ -1,5 +1,40 @@
-export default (state = {}, action) => {
+const initialState = {
+  filter: {
+    status: 'all',
+    owner: 'all',
+  },
+  sortValue: 'newest',
+}
+
+const CHANGE_FILTER = 'filters/CHANGE_FILTER'
+const CHANGE_SORT = 'filters/CHANGE_SORT'
+
+export const changeFilter = (filterKey, value) => ({
+  type: CHANGE_FILTER,
+  filterKey,
+  value,
+})
+
+export const changeSort = value => ({
+  type: CHANGE_SORT,
+  value,
+})
+
+export default (state = initialState, action) => {
   switch (action.type) {
+    case CHANGE_FILTER:
+      return {
+        ...state,
+        filter: {
+          ...state.filter,
+          [action.filterKey]: action.value,
+        },
+      }
+    case CHANGE_SORT:
+      return {
+        ...state,
+        sortValue: action.value,
+      }
     default:
       return state
   }
