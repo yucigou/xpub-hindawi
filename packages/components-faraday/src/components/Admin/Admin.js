7e4b7e6b149ce76ad87ca3b9954cd65bc999a439
Bogdan Cochior <bogdan.cochior@thinslices.com>
2018-02-15 10:01:36 +0200

    fix(component): fix typo

--- packages/components-faraday/src/components/Admin/Admin.js
+++ packages/components-faraday/src/components/Admin/Admin.js
@@ -33,7 +33,7 @@ const Admin = ({
   <div>
     <Header>
       <span>Users</span>
-      <AddButton onClick={() => history.push('/admin/urers/add')}>
+      <AddButton onClick={() => history.push('/admin/users/add')}>
         <Icon color="#667080">plus-circle</Icon>
         Add User
       </AddButton>
