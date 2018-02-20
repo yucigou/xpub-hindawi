810441383eeb65ee3a70a0bd091194846346ff38
Alexandru Munteanu <alexandru.munteanu@thinslices.com>
2018-02-20 13:03:45 +0200

    create withModal hoc

---
+++ packages/component-modal/src/components/Modal.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+
+const createModalRootElement = () => {
+  const modalDiv = document.createElement('div')
+  modalDiv.setAttribute('id', 'ps-modal-root')
+  document.body.insertAdjacentElement('beforeend', modalDiv)
+
+  return modalDiv
+}
+
+const modalRoot = createModalRootElement()
+
+class Modal extends React.Component {
+  render() {
+    const { component: Component, overlayColor, ...rest } = this.props
+    return ReactDOM.createPortal(
+      <ModalRoot overlayColor={overlayColor}>
+        <Component {...rest} />
+      </ModalRoot>,
+      modalRoot,
+    )
+  }
+}
+
+export default Modal
+
+const ModalRoot = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ overlayColor }) =>
+    overlayColor || 'rgba(0, 0, 0, 0.8)'};
+`
--- packages/components-faraday/src/components/Dashboard/Dashboard.js
+++ packages/components-faraday/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { get } from 'lodash'
 import { Button } from '@pubsweet/ui'
+import styled from 'styled-components'
 import { compose, withHandlers } from 'recompose'
+import { withModal } from 'pubsweet-component-modal/src/components'
 
 import classes from './Dashboard.local.scss'
 import AbstractModal from './AbstractModal'
@@ -23,6 +25,7 @@ const Dashboard = ({
   filterItems,
   abstractModal,
   setModal,
+  showModal,
   ...rest
 }) => (
   <div className={classes.root}>
@@ -31,6 +34,17 @@ const Dashboard = ({
       <div className={classes.headerButtons}>
         <Button onClick={createDraftSubmission} primary>
           New
+        </Button>
+        <Button
+          onClick={() =>
+            showModal({
+              onConfirm: () => alert('confirm'),
+              costel: true,
+            })
+          }
+          primary
+        >
+          Show modal
         </Button>
       </div>
     </div>
@@ -49,7 +63,28 @@ const Dashboard = ({
   </div>
 )
 
+const ModalRoot = styled.div`
+  width: 300px;
+  height: 400px;
+  background-color: gray;
+`
+
+const ModalComponent = ({ hideModal, dismissable, onConfirm, costel }) => (
+  <ModalRoot onClick={dismissable ? hideModal : null}>
+    This is our custom modal component.
+    <div>
+      <button onClick={onConfirm}>Yes</button>
+      <button onClick={hideModal}>Hide modal</button>
+      <span>{costel ? 'e costel' : 'nu e'}</span>
+    </div>
+  </ModalRoot>
+)
+
 export default compose(
+  withModal({
+    modalComponent: ModalComponent,
+    overlayColor: 'rgba(255, 0,0,0.8)',
+  }),
   withHandlers({
     getItems: ({
       filters,
