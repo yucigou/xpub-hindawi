dfb9c716ae9d582df67d49119c6f80b2165472ba
Sebastian <sebastian.mihalache@thinslices.com>
2018-02-21 14:16:54 +0200

    add full create user tests

--- packages/component-invite/src/tests/fixtures/users.js
+++ packages/component-invite/src/tests/fixtures/users.js
@@ -7,4 +7,22 @@ module.exports = {
     admin: true,
     id: 'admin123',
   },
+  editorInChief: {
+    type: 'user',
+    username: 'editor',
+    email: 'editor@example.com',
+    password: 'test',
+    admin: false,
+    id: 'editor123',
+    roles: ['editorInChief'],
+  },
+  handlingEditor: {
+    type: 'user',
+    username: 'handling',
+    email: 'handling@example.com',
+    password: 'test',
+    admin: false,
+    id: 'handling123',
+    roles: ['handlingEditor'],
+  },
 }
